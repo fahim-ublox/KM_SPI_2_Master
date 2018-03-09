4b14e357ce9bc1e03409d3ed6b043502a0de6be1
Wajahat Abbas <wabb@4mwirelesspk.local>
2018-03-09 19:08:38 +0500

    spi example

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include "mbed.h"
 #include "rtos.h"
 
-Serial pc(UART1_TX, UART1_RX);
-
-
+SPI spi(SPI1_MOSI, SPI1_MISO, SPI1_CLK, SPI1_CS); // mosi, miso, sclk
+DigitalOut cs(SPI1_CS);
 
 int main() {
-    pc.printf("KM SPI Starts\r\n");
+    int x = 0, y = 0;
 
+    printf("KM SPI Starts\r\n");
+
+    cs=0;
+    while(true) {
+        x=spi.write(i);
+        y++;
+        if (y > 255) {
+            break;
+        }
+    }
+    cs=1;   
 }
+           
