b68012155d0b65c59b069c302d06fcec4043d711
Wajahat Abbas <wabb@4mwirelesspk.local>
2018-03-09 19:18:23 +0500

    SPI example

--- main.cpp
+++ main.cpp
@@ -5,18 +5,17 @@ SPI spi(SPI1_MOSI, SPI1_MISO, SPI1_CLK, SPI1_CS); // mosi, miso, sclk
 DigitalOut cs(SPI1_CS);
 
 int main() {
-    int x = 0, y = 0;
+    int i = 0;
 
     printf("KM SPI Starts\r\n");
 
     cs=0;
     while(true) {
-        x=spi.write(i);
-        y++;
-        if (y > 255) {
+        spi.write(i);
+        i++;
+        if (i > 255) {
             break;
         }
     }
-    cs=1;   
+    cs=1;
 }
-           
